7791ca477a6d3527b2404692b684dcea7050db07
Gordus Alexander <aldrmiller@gmail.com>
2025-09-20 18:24:00 +0200

    add autoprefixer and setting

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,17 @@ const rename = require("gulp-rename");
 const sass = require("gulp-sass")(require("sass"));
 const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
+const autoprefixer = require("gulp-autoprefixer").default;
 
 function buildScssFromCss() {
-    return gulp.src("./scss/style.scss")
+    return gulp
+        .src("./scss/style.scss")
         .pipe(sass().on("error", sass.logError))
-        .pipe(postcss([cssnano()])) //
+        .pipe(postcss([cssnano()]))
+        .pipe(autoprefixer({
+            cascade: false,
+            overrideBrowserslist: ["last 2 versions", "> 1%", "ie 11"]
+        }))
         .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest("./css/"));
 }
